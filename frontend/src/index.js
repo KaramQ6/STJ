bd25464d998106c1abb507bccfe6cd76b8683499
Karam Khasawneh <202410447@std.jadara.edu.jo>
2025-07-19 13:30:14 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -23,7 +22,5 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
-reportWebVitals();
 
 console.log(" React app loaded successfully!");
